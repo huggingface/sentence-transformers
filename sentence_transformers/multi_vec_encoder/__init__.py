ac0cf2eabf8b4c7839905e357e7a70206a8cdc08
AymenKallala <aymen.kallala@gmail.com>
2026-01-23 19:41:43 +0100

    fix circular import bug

--- sentence_transformers/multi_vec_encoder/__init__.py
+++ sentence_transformers/multi_vec_encoder/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from sentence_transformers.multi_vec_encoder.MultiVectorEncoder import MultiVectorEncoder
+# LateInteractionPooling must be imported first to avoid circular import
+# (MultiVectorEncoder imports LateInteractionPooling from this package)
 from sentence_transformers.multi_vec_encoder.LateInteractionPooling import LateInteractionPooling
+from sentence_transformers.multi_vec_encoder.MultiVectorEncoder import MultiVectorEncoder
 
 
 __all__ = [
